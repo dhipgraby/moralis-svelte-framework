0603b770e2185f85f23d42dc2e358d40ad7a2a89
dhipgraby <kenneth.zambrano4@gmail.com>
2021-05-28 03:31:12 +0200

    Event msg settings and handlers
    Add messages when get oracle response with generated random number

--- src/components/alerts/alertMsg.svelte
+++ src/components/alerts/alertMsg.svelte
@@ -1,21 +1,43 @@
-
 <script>
-import { fly } from 'svelte/transition';
-export let alertType = "info";
-export let content = "This is a empty alert, just close it...";
-export let show = true0
-
+  import { fly } from "svelte/transition";
+  export let alertType = "info";
+  export let content = "This is a empty alert, just close it...";
+  export let show = true;
+  
 </script>
 
 {#if show}
-   
-<div class="alert alert-{alertType} alert-dismissible fade show" role="alert" transition:fly="{{ y: 100, duration: 2000 }}" >
-  <p>{@html content}</p>
- <button type="button" class="btn-close" on:click={()=>{ show = false }}></button>
-</div>
+  <div id="alertDiv">
+    <div
+      class="alert alert-{alertType} alert-dismissible fade show"
+      role="alert"
+      transition:fly={{ y: 100, duration: 700 }}
+    >
+      <p>{@html content}</p>
 
+      <button
+        type="button"
+        class="btn-close"
+        on:click={() => {
+          show = false;
+        }}
+      />
+    </div>
+  </div>
 {/if}
 
-  <style>
-    p { margin: 0;}
-  </style>
+<style>
+
+#alertDiv {
+
+position:fixed;
+bottom: 10px;
+width: fit-content;
+
+}
+
+
+  p {
+    margin: 0;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,13 +3,12 @@
 	import { alertTrigger } from "@storage/global";
 	import Alert from "../components/alerts/alertMsg.svelte";
 	import TableContainer from "../components/tables/TableContainer.svelte";
- 	import ContractButtons from "../components/contract/ContractButtons.svelte";
+	import ContractButtons from "../components/contract/ContractButtons.svelte";
 	import { initEventListener } from "@contracts/events";
-	
-	
-	onMount(async () =>{
-		await initEventListener()
-	})
+
+	onMount(async () => {
+		await initEventListener();
+	});
 
 	let alertData;
 
@@ -20,25 +19,29 @@
 			content: value.content,
 		};
 	});
-
 </script>
 
 <svelte:head>
 	<title>Moralis CoinFlip App</title>
 </svelte:head>
 
+{#if alertData.show}
+<Alert {...alertData} />
+{/if}
+
+
 <div class="mt-4" align="center">
-	<h1 class="c-yellow reflex">Ethereum Betting Dice <i class="fab fa-ethereum" /></h1>
-	<p class="pl-5 pr-5">Bet for  a lucky number Higher than 51 or Lower than 49 and get 40% profit from your bets </p>
-
-	{#if alertData.show}
-		<div class="pt-3">
-			<Alert {...alertData} />
-		</div>
-	{/if}
-
+	<h1 class="c-yellow reflex">
+		Ethereum Betting Dice <i class="fab fa-ethereum" />
+	</h1>
+	<p class="pl-5 pr-5">
+		Bet for a lucky number Higher than 51 or Lower than 49 and get 40%
+		profit from your bets
+	</p>
+	
 	<div class="btnDiv" align="center">
 		<br />
+	
 		<ContractButtons />
 		<TableContainer />
 	</div>
