f90afac5aac87c6ebd82e20655d6cc8b3394cc57
kennow <kenneth.zambrano4@gmail.com>
2022-03-22 03:25:28 +0100

    Sell options and contract changes

--- src/components/dragon/SellOption.svelte
+++ src/components/dragon/SellOption.svelte
@@ -70,10 +70,10 @@
         <div align="center">
             <h6>Approve the marketplace to sell your NFTs</h6>
             <p><small>Approve marketplace for only this token</small></p>
-            <button class="btn btn-danger text-light" on:click={() => singleApproval(dragonProps.id) }>Approve single token</button>
+            <button class="btn btn-danger text-light" data-bs-dismiss="modal" on:click={() => singleApproval(dragonProps.id) }>Approve single token</button>
             <br>
             <p><small> Approve marketplace for your tokens</small></p>
-            <button class="btn btn-danger text-light" on:click={() => setApprovalForAll() }>Approve for All</button>
+            <button class="btn btn-danger text-light" data-bs-dismiss="modal" on:click={() => setApprovalForAll() }>Approve for All</button>
         </div>
     {/if}
 </BasicModal>
